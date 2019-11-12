296ca7feab9fc4ea829a6979e0f1ddf32f56f699
Yash <yashsk@protonmail.com>
2019-11-12 15:50:43 +1100

    Added UserId

--- routes/singUp/signUp.js
+++ routes/singUp/signUp.js
@@ -2,12 +2,14 @@ const express = require('express');
 const signUpRoutes = express.Router();
 const validation = require('../../utils/validations/singUpValidation.js');
 const moment = require('moment');
+const uuid = require('uuid/v4');
 
 signUpRoutes.post("/",validation,async(req,res,next) => {
 	let subStatus = req.body.edm || false ;
 	res.status(201).json({
 		"Data": req.body,
 		"Message": "User Created Successfully.",
+		"UserId" : uuid(),
 		"CreatedAt": moment.now(),
 		"NextSteps":"Please verify your email address",
 		"EmailValidation" : "Pending",
