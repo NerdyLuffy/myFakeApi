13ca72dfe779087384a460552da13a22d03c0ac4
Yash <yashsk@protonmail.com>
2019-11-13 21:16:29 +1100

    Now deployment config and API route changes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
 let app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(morgan('dev'));
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../public/')));
 
 //Routes
 const singUpRoutes = require('../routes/singUp/signUp.js');
@@ -15,10 +19,14 @@ const sessionRoutes = require('../routes/session.js');
 const contactUsRoute = require('../routes/contactUs/contactUs.js');
 const footballRoutes = require('../routes/football/football.js');
 
-app.use('/signup', singUpRoutes);
-app.use('/login', loginRouter);
-app.use('/session', sessionRoutes);
-app.use('/contactus', contactUsRoute);
-app.use('/football', footballRoutes);
+app.get('/',(req,res) => {
+	res.sendFile(path.join(__dirname+'../public/index.html'));
+})
+
+app.use('/api/signup', singUpRoutes);
+app.use('/api/login', loginRouter);
+app.use('/api/session', sessionRoutes);
+app.use('/api/contactus', contactUsRoute);
+app.use('/api/football', footballRoutes);
 
 module.exports = app;
