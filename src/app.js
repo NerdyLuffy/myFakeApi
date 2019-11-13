eea605d6844607eeb49826804f08e04fd6cf19d0
Angad <angadraman1@gmail.com>
2019-11-13 23:48:48 +1100

    chore: cleanup

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const morgan = require("morgan");
+const path = require("path");
 let app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../public/')));
+// app.use(express.static(path.join(__dirname, '../public/')));
 
 //Routes
-const singUpRoutes = require('../routes/singUp/signUp.js');
-const loginRouter = require('../routes/logIn/logIn.js');
-const sessionRoutes = require('../routes/session.js');
-const contactUsRoute = require('../routes/contactUs/contactUs.js');
-const footballRoutes = require('../routes/football/football.js');
-const carRoutes = require('../routes/cars/cars.js');
-const usersRoutes = require('../routes/users/users.js');
+const singUpRoutes = require("../routes/singUp/signUp.js");
+const loginRouter = require("../routes/logIn/logIn.js");
+const sessionRoutes = require("../routes/session.js");
+const contactUsRoute = require("../routes/contactUs/contactUs.js");
+const footballRoutes = require("../routes/football/football.js");
+const carRoutes = require("../routes/cars/cars.js");
+const usersRoutes = require("../routes/users/users.js");
 
-app.get('/',(req,res) => {
-	res.sendFile(path.join(__dirname+'../public/index.html'));
-})
+// app.get('/',(req,res) => {
+// 	res.sendFile(path.join(__dirname+'../public/index.html'));
+// })
 
-app.use('/api/signup', singUpRoutes);
-app.use('/api/login', loginRouter);
-app.use('/api/session', sessionRoutes);
-app.use('/api/contactus', contactUsRoute);
-app.use('/api/football', footballRoutes);
-app.use('/api/cars',carRoutes);
-app.use('/api/users',usersRoutes);
+app.use("/api/signup", singUpRoutes);
+app.use("/api/login", loginRouter);
+app.use("/api/session", sessionRoutes);
+app.use("/api/contactus", contactUsRoute);
+app.use("/api/football", footballRoutes);
+app.use("/api/cars", carRoutes);
+app.use("/api/users", usersRoutes);
 
 module.exports = app;
