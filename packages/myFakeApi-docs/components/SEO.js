a209a572224681ff2ab5d2e6350fbc7f43868333
angad777 <39453522+angad777@users.noreply.github.com>
2019-11-28 11:38:46 +1100

    feat: added openGraph

--- packages/myFakeApi-docs/components/SEO.js
+++ packages/myFakeApi-docs/components/SEO.js
@@ -6,9 +6,9 @@ const SEO = () => (
 		<NextSeo
 			title="My Fake API "
 			description="Kickstart new applications at light speed using our modular and free to use API mocking service that gives you all the essential endpoints you would require to scaffold, mock and test your applications. Get a full REST API without any registration for testing and prototyping your application. Nodejs and React lovers. No more waiting for BE to create sample API, we've got it covered. REST API at its Best. No registration required. Serverless REST API"
-			canonical="https://www.canonical.ie/"
+			canonical="https://www.myfakeapi.com/"
 			openGraph={{
-				url: 'https://www.url.ie/a',
+				url: 'https://www.myfakeapi.com/',
 				title: 'My Fake API',
 				description:
 					'Kickstart new applications at light speed using our modular and free to use API mocking service that gives you all the essential endpoints you would require to scaffold, mock and test your applications. Get a full REST API without any registration for testing and prototyping your application. Nodejs and React lovers. No more waiting for BE to create sample API, we have it covered. REST API at its Best. No registration required. Serverless REST API',
