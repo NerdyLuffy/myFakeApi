8f16fc969908518096b0f7ca98a43f725230871b
angad777 <39453522+angad777@users.noreply.github.com>
2019-12-04 10:22:40 +1100

    perf: routes and utils lint

--- utils/validations/contactUsValidation.js
+++ utils/validations/contactUsValidation.js
@@ -2,49 +2,38 @@ const _ = require('lodash');
 
 module.exports = contactUsValidation;
 
-async function contactUsValidation(req,res,next)
-{
+async function contactUsValidation(req, res, next) {
 	let errors = [];
-	if(req.body)
-	{
-		if(!_.isString(req.body.firstName))
-		{
+	if (req.body) {
+		if (!_.isString(req.body.firstName)) {
 			errors.push({
-				"reason":"firstName",
-				"errorMessage":"firstName is required."
+				reason: 'firstName',
+				errorMessage: 'firstName is required.'
 			});
 		}
-		if(!_.isString(req.body.lastName))
-		{
+		if (!_.isString(req.body.lastName)) {
 			errors.push({
-				"reason":"lastName",
-				"errorMessage":"lastName is required."
-			});
-			
-		}
-		if(!_.isString(req.body.message))
-		{
-			errors.push({
-				"reason":"message",
-				"errorMessage":"Message is required."
-			});
-			
-		}
-		if(!_.isNumber(req.body.contact))
-		{
-			errors.push({
-				"reason":"contact",
-				"errorMessage":"Please provide valid Contact number."
+				reason: 'lastName',
+				errorMessage: 'lastName is required.'
 			});
 		}
-		if(errors.length > 0)
-		{
+		if (!_.isString(req.body.message)) {
+			errors.push({
+				reason: 'message',
+				errorMessage: 'Message is required.'
+			});
+		}
+		if (!_.isNumber(req.body.contact)) {
+			errors.push({
+				reason: 'contact',
+				errorMessage: 'Please provide valid Contact number.'
+			});
+		}
+		if (errors.length > 0) {
 			res.status(400).json({
-				"Errors": errors
+				Errors: errors
 			});
-		}
-		else
-		{
+		} else {
 			return next();
 		}
 	}
